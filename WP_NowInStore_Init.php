08a799e0a4efea72fc026c4c78165acf7fc6cbe7
nowinstore <nowinstore@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 01:14:38 +0000

    added support for new version of wordpresds
    git-svn-id: https://plugins.svn.wordpress.org/now-in-store-catalog-builder/tags/1.0.4@1211498 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ WP_NowInStore_Init.php
@@ -0,0 +1,145 @@
+<?php
+
+class NowInStore_CatalogBuilder
+{
+    public function __construct()
+    {
+      add_filter( 'plugin_action_links_'. plugin_basename( __FILE__ ), array($this,'plugin_action_links') );
+      add_action( 'init', array($this,'add_rewrite_rules'));
+      add_filter( 'query_vars', array($this,'add_query_vars' ));
+      register_activation_hook( __FILE__, array($this,'wpa_install') );
+      register_deactivation_hook( __FILE__, array($this,'wpa_uninstall')  );
+      add_action( 'template_redirect', array($this,'add_template_redirect'));
+    }
+
+    public function plugin_action_links( $actions )
+    {
+      $baseUrl = urlendcode (get_site_url().'/');
+    	$actions[] = '<a href="https://www.nowinstore.com/auth/woocommerce/callback?baseUrl='.$baseUrl.'">Open</a>';
+    	return $actions;
+    }
+
+    public function add_rewrite_rules() {
+      // add_rewrite_endpoint('nowinstore', EP_PERMALINK | EP_PAGES );
+      add_rewrite_rule('index.php/catalogbuilder/(.+?)/(.+?)?$', 'index.php?nowinstore_resource=$matches[1]&action=$matches[2]', 'top');
+      add_rewrite_rule('index.php/catalogbuilder/(.+?)?$', 'index.php?nowinstore_resource=$matches[1]', 'top');
+      add_rewrite_rule('catalogbuilder/(.+?)/(.+?)?$', 'index.php?nowinstore_resource=$matches[1]&action=$matches[2]', 'top');
+      add_rewrite_rule('catalogbuilder/(.+?)?$', 'index.php?nowinstore_resource=$matches[1]', 'top');
+           flush_rewrite_rules();
+    }
+
+    public function add_query_vars($vars){
+        $vars[] = "nowinstore_resource";
+        $vars[] = "page";
+        $vars[] = "category_id";
+        $vars[] = "keywords";
+        $vars[] = "action";
+        return $vars;
+    }
+
+    public function wpa_install() {
+         flush_rewrite_rules();
+    }
+
+    public function wpa_uninstall() {
+      flush_rewrite_rules();
+    }
+
+    public function add_template_redirect() {
+
+      $page = (get_query_var('page')) ? get_query_var('page') : 1;
+      	if ( get_query_var( 'nowinstore_resource' ) == 'products' ) {
+    	    header( 'Content-Type: application/json' );
+
+          $args = array( 'post_type' => 'product', 'posts_per_page' => 50,
+            'orderby' => 'title', 'order'   => 'ASC', 'paged' => $page );
+            if (get_query_var('category_id')) {
+              $cat = get_term( get_query_var('category_id'), 'product_cat' );
+              $args["product_cat"] = $cat->slug;
+            }
+
+            if (get_query_var('keywords')) {
+              $args["s"] = get_query_var('keywords');
+            }
+          $loop = new WP_Query( $args );
+
+
+          if (get_query_var('action') == 'count') {
+            echo json_encode(array("count" => intval($loop->found_posts)));
+            exit;
+          } else {
+
+            $iso_currency_code = get_woocommerce_currency();
+            $products = array();
+            while ( $loop->have_posts() ) : $loop->the_post();
+            global $product;
+            // $attributes = $product->get_attributes();
+            // $variations = $product->get_available_variations();
+            // $formatted_attributes = array();
+            // foreach($attributes as $attr=>$attr_deets){
+            //
+            //     $attribute_label = wc_attribute_label($attr);
+            //
+            //     if ( isset( $attributes[ $attr ] ) || isset( $attributes[ 'pa_' . $attr ] ) ) {
+            //
+            //         $attribute = isset( $attributes[ $attr ] ) ? $attributes[ $attr ] : $attributes[ 'pa_' . $attr ];
+            //
+            //         if ( $attribute['is_taxonomy'] ) {
+            //
+            //             $formatted_attributes[$attribute_label] = implode( ', ', wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) ) );
+            //
+            //         } else {
+            //
+            //             $formatted_attributes[$attribute_label] = $attribute['value'];
+            //         }
+            //
+            //     }
+            // }
+            $main_image = wp_get_attachment_image_src( $product->get_image_id(), "full" );
+            $thumbnail_image = wp_get_attachment_image_src( $product->get_image_id(), array(75,75) );
+            array_push($products, array(
+                    "id" => get_the_ID(),
+                    "title" => $product->get_title(),
+                    "sku" => $product->get_sku(),
+                    "price" => $product->get_price(),
+                    "main_image" =>  $main_image[0],
+                    "thumbnail_image" =>  $thumbnail_image[0],
+                    "iso_currency_code" => $iso_currency_code,
+                    "url" => get_permalink(),
+                    "variations" => $formatted_attributes
+            ));
+            endwhile;
+
+            wp_reset_query();
+            echo json_encode($products);
+            exit;
+        }
+      } else if ( get_query_var( 'nowinstore_resource' ) == 'categories' ) {
+        header( 'Content-Type: application/json' );
+        $categories = array();
+        $product_categories = get_terms( 'product_cat', array('orderby' => 'name', 'order'   => 'ASC', 'hide_empty' => true) );
+        foreach($product_categories as $product_category) {
+          if ($product_category->parent != 0) {
+            array_push($categories, array(
+                    "id" => $product_category->term_id,
+                    "title" => $product_category->name
+            ));
+          }
+        }
+        echo json_encode($product_categories);
+        exit;
+      } else if ( get_query_var( 'nowinstore_resource' ) == 'profile' ) {
+        header( 'Content-Type: application/json' );
+        echo json_encode(array(
+                "business_name" => get_option('blogname'),
+                "email" => get_option('admin_email'),
+                "baseUrl" =>  get_site_url().'/'
+        ));
+        exit;
+      }
+    }
+}
+
+
+
+?>
---
+++ nowinstore.php
@@ -0,0 +1,24 @@
+<?php
+   /*
+   Plugin Name: WooCommerce - Now In Store Catalog Builder
+   Plugin URI: http://www.nowinstore.com
+   Description: Create beautiful product catalogs that you can print or share online.
+   Version: 1.0.4
+   Author: Now In Store Inc.
+   Author URI: https://www.nowinstore.com
+   License: GPL2
+   */
+   error_reporting(E_ALL);
+   ini_set('display_errors', 1);
+   require_once dirname(__FILE__) . '/WP_NowInStore_Init.php';
+   function plugin_action_links( $actions )
+    {
+      $baseUrl = urlencode (get_site_url().'/');
+      $actions[] = '<a href="https://www.nowinstore.com/auth/woocommerce/callback?baseUrl='.$baseUrl.'" target="_blank">Open</a>';
+      return $actions;
+    }
+    add_filter( 'plugin_action_links_'. plugin_basename( __FILE__ ), 'plugin_action_links' );
+
+   new NowInStore_CatalogBuilder();
+
+?>
